464b8e0cbf5b60bb1250cc0d1588eaae54e1992e
greatxrider <daligdig.jephmari@gmail.com>
2024-09-08 00:31:06 +0800

    Add cookies and smart redirect for app

--- codeAlong/react-authentication/client/src/components/UserSignOut.js
+++ codeAlong/react-authentication/client/src/components/UserSignOut.js
@@ -1,13 +1,15 @@
-import { useContext, useEffect } from "react";
-import UserContext from "../context/UserContext";
-import { Navigate } from "react-router-dom";
+import { useEffect, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import UserContext from '../context/UserContext';
 
 const UserSignOut = () => {
     const { actions } = useContext(UserContext);
 
     useEffect(() => actions.signOut());
 
-    return <Navigate to="/" replace />
-}
+    return (
+        <Navigate to="/" replace />
+    );
+};
 
-export default UserSignOut
+export default UserSignOut;
